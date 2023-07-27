e2e0d0a7bfa7c0765b5dd9ed729918a426612384
Job Henandez Lara <hj93@protonmail.com>
2023-07-26 20:42:12 -0700

    include h files

--- src/main.c
+++ src/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "matrix.h"
+#include "vector.h"
+#include "polynomial.h"
+#include "matrix_vec.h"
 
 int main() {
   printf("Everything compiled!\n");
